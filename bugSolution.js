8edd55e95610faf45b001f3c606d53cad3abb676
BugHunter-X <askzenai@gmail.com>
2025-01-04 21:26:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,11 @@
+```javascript
+// This is the corrected code
+<div className="flex">
+  <div className="bg-red-500 p-4 flex-1">
+    This is a div
+  </div>
+  <div className="bg-blue-500 p-4 flex-1">
+    This is another div
+  </div>
+</div>
+```
